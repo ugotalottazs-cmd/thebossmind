45afa85aea3a5668e04746e8569afeeb126132c4
ugotalottazs-cmd <ugotalottazs@gmail.com>
2026-07-17 06:26:36 -0500

    Add category and tag page generator plugin

---
+++ _plugins/category_generator.rb
@@ -0,0 +1,59 @@
+module Jekyll
+  class CategoryPage < Page
+    def initialize(site, base, dir, category)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'category.html')
+      self.data['category'] = category
+
+      category_title_prefix = site.config['category_title_prefix'] || 'Category: '
+      self.data['title'] = "#{category_title_prefix}#{category}"
+    end
+  end
+
+  class CategoryPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'category'
+        dir = site.config['category_dir'] || 'categories'
+        site.categories.each_key do |category|
+          site.pages << CategoryPage.new(site, site.source, File.join(dir, category), category)
+        end
+      end
+    end
+  end
+
+  class TagPage < Page
+    def initialize(site, base, dir, tag)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      self.data['tag'] = tag
+
+      tag_title_prefix = site.config['tag_title_prefix'] || 'Tag: '
+      self.data['title'] = "#{tag_title_prefix}#{tag}"
+    end
+  end
+
+  class TagPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'tag'
+        dir = site.config['tag_dir'] || 'tags'
+        site.tags.each_key do |tag|
+          site.pages << TagPage.new(site, site.source, File.join(dir, tag), tag)
+        end
+      end
+    end
+  end
+end
